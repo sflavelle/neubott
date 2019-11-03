39b5574a1cc1b1aa4e30f20ab27857da6afb7a53
Simon Flavelle <me@neurario.com>
2019-11-03 08:47:19 +0000

    Using axios.get in place of http.get

--- commands/splatoon.js
+++ commands/splatoon.js
@@ -1,7 +1,6 @@
 const Chariot = require('chariot.js');
 const FS = require("fs");
-const http = require('https');
-const request = require("request");
+const axios = require("axios");
 
 class Splatoon extends Chariot.Command {
     constructor() {
@@ -19,61 +18,47 @@ class Splatoon extends Chariot.Command {
     }
 
     async execute(message, args, chariot) {
+
         let TSbegin = Date.now();
-        // Let's try to make a cache here
-        const cacheBattle = ( FS.existsSync('./resources/.cache/spl2-battle.json') ) ? JSON.parse(FS.readFileSync('./resources/.cache/spl2-battle.json', 'utf8')) : null;
-        if (cacheBattle == null || cacheBattle.regular[0].end_time < Date.now()/1000) {
-        const options = {
-            host: 'splatoon2.ink',
-            path: '/data/schedules.json',
-            family: 4,
+
+        let options = {
+            baseURL: 'https://splatoon2.ink/',
             headers: {
                 'User-Agent': 'Neubott/0.1 (Neu#1909, @Splatsune)'
             }
         }
 
-        const req = http.get(options, (res) => {
-            // Chariot.Logger.event(`Splatoon API status code: ${res.statusCode}`);
-            // Chariot.Logger.event(`Splatoon API headers: ${JSON.stringify(res.headers)}`);
-            res.setEncoding('utf8');
-            let rawSchedules = '';
-            res.on('data', (chunk) => {
-                rawSchedules += chunk;
-            });
-            res.on('end',() => {
-                try {
-                const parsedSchedules = JSON.parse(rawSchedules);
-                FS.writeFileSync('./resources/.cache/spl2-battle.json', JSON.stringify(parsedSchedules, null, 2));
-                var timeEndBattle = new Date(parsedSchedules.regular[0].end_time);
-                var timeRightNow = Math.floor(Date.now()/1000);
-                var timeLeftBattle = Math.floor(Math.abs(timeEndBattle - timeRightNow) /60);
-                let TSend = Date.now();
+        // Let's try to make a cache here
+        const cacheBattle = ( FS.existsSync('./resources/.cache/spl2-battle.json') ) ? JSON.parse(FS.readFileSync('./resources/.cache/spl2-battle.json', 'utf8')) : null;
+        if (cacheBattle == null || cacheBattle.regular[0].end_time < Date.now()/1000) {
+
+        const reqBattle = await axios.get('/data/schedules.json', options);
+        const dataBattle = reqBattle.data;
+
+        FS.writeFileSync('./resources/.cache/spl2-battle.json', JSON.stringify(dataBattle, null, 2));
+        var timeEndBattle = new Date(dataBattle.regular[0].end_time);
+        var timeRightNow = Math.floor(Date.now()/1000);
+        var timeLeftBattle = Math.floor(Math.abs(timeEndBattle - timeRightNow) /60);
+        let TSend = Date.now();
                 // Chariot.Logger.event("Current Turf War Stages: " + parsedSchedules.regular[0].stage_a.name + " & " + parsedSchedules.regular[0].stage_b.name);
                 // Chariot.Logger.event("Current Ranked " + parsedSchedules.gachi[0].rule.name + " Stages: " + parsedSchedules.gachi[0].stage_a.name + " & " + parsedSchedules.gachi[0].stage_b.name);
                 // Chariot.Logger.event("Current League " + parsedSchedules.league[0].rule.name + " Stages: " + parsedSchedules.league[0].stage_a.name + " & " + parsedSchedules.league[0].stage_b.name);
                 // Chariot.Logger.event("This rotation will change in: " + timeLeftBattle + " minutes");
 
-                message.channel.createEmbed(new Chariot.RichEmbed()
-                    .setColor('ORANGE')
-                    .setTitle('Splatoon 2: Current Stages')
-                    .setDescription('This schedule is valid for the next **' + timeLeftBattle + '** minutes.')
-                    .addField('Turf War', parsedSchedules.regular[0].stage_a.name + '\n' + parsedSchedules.regular[0].stage_b.name, true)
-                    .addField('Ranked (' + parsedSchedules.gachi[0].rule.name + ')', parsedSchedules.gachi[0].stage_a.name + '\n' + parsedSchedules.gachi[0].stage_b.name,true)
-                    .addField('League (' + parsedSchedules.league[0].rule.name + ')', parsedSchedules.league[0].stage_a.name + '\n' + parsedSchedules.league[0].stage_b.name,true)
-                    .setUrl('https://splatoon2.ink/')
-                    .setTimestamp()
-                    .setFooter('Data provided by Splatoon2.ink, processed in ' + (TSend - TSbegin) + 'ms','https://splatoon2.ink/favicon-32x32.png')
-                    );
-            } catch (e) { message.channel.createMessage("❌ Problem with request: `" + e.message + "`"); }
-            });
-        });
+        message.channel.createEmbed(new Chariot.RichEmbed()
+            .setColor('ORANGE')
+            .setTitle('Splatoon 2: Current Stages')
+            .setDescription('This schedule is valid for the next **' + timeLeftBattle + '** minutes.')
+            .setImage('https://splatoon2.ink/assets/splatnet' + dataBattle.regular[0].stage_a.image)
+            .addField('Turf War', dataBattle.regular[0].stage_a.name + '\n' + dataBattle.regular[0].stage_b.name, true)
+            .addField('Ranked (' + dataBattle.gachi[0].rule.name + ')', dataBattle.gachi[0].stage_a.name + '\n' + dataBattle.gachi[0].stage_b.name,true)
+            .addField('League (' + dataBattle.league[0].rule.name + ')', dataBattle.league[0].stage_a.name + '\n' + dataBattle.league[0].stage_b.name,true)
+            .setUrl('https://splatoon2.ink/')
+            .setTimestamp()
+            .setFooter('Data provided by Splatoon2.ink, processed in ' + (TSend - TSbegin) + 'ms','https://splatoon2.ink/favicon-32x32.png')
+            );
 
-        req.on('error', (e) => {
-            message.channel.createMessage("❌ Problem with request: `" + e.message + "`");
-          });
-
-        req.end();
-        let pause = await req;
+        let pause = await reqBattle;
         }
         else { // Use the cached data
             var timeEndBattle = new Date(cacheBattle.regular[0].end_time);
@@ -89,6 +74,7 @@ class Splatoon extends Chariot.Command {
                 .setColor('ORANGE')
                 .setTitle('Splatoon 2: Current Stages')
                 .setDescription('This schedule is valid for the next **' + timeLeftBattle + '** minutes.')
+                .setImage('https://splatoon2.ink/assets/splatnet' + cacheBattle.regular[0].stage_a.image)
                 .addField('Turf War', cacheBattle.regular[0].stage_a.name + '\n' + cacheBattle.regular[0].stage_b.name, true)
                 .addField('Ranked (' + cacheBattle.gachi[0].rule.name + ')', cacheBattle.gachi[0].stage_a.name + '\n' + cacheBattle.gachi[0].stage_b.name,true)
                 .addField('League (' + cacheBattle.league[0].rule.name + ')', cacheBattle.league[0].stage_a.name + '\n' + cacheBattle.league[0].stage_b.name,true)
@@ -99,81 +85,68 @@ class Splatoon extends Chariot.Command {
         }
         // Now for the Salmon Run data
         TSbegin = Date.now();
+        let dataSRG = [];
         const cacheSR = ( FS.existsSync('./resources/.cache/spl2-salmonrun.json') ) ? JSON.parse(FS.readFileSync('./resources/.cache/spl2-salmonrun.json', 'utf8')) : null;
+        const cacheSRG = ( FS.existsSync('./resources/.cache/spl2-srgear.json') ) ? JSON.parse(FS.readFileSync('./resources/.cache/spl2-srgear.json', 'utf8')) : null;
         if (cacheSR == null || cacheSR.details[0].end_time < Date.now()/1000) {
-            const options = {
-                host: 'splatoon2.ink',
-                path: '/data/coop-schedules.json',
-                family: 4,
-                headers: {
-                    'User-Agent': 'Neubott/0.1 (Neu#1909, @Splatsune)'
-                }
-            }
-        
-        const req2 = http.get(options, (res) => {
-            // Chariot.Logger.event(`Splatoon API status code: ${res.statusCode}`);
-            // Chariot.Logger.event(`Splatoon API headers: ${JSON.stringify(res.headers)}`);
-            res.setEncoding('utf8');
-            let rawSchedules = '';
-            res.on('data', (chunk) => {
-                rawSchedules += chunk;
-            });
-            res.on('end',() => {
-                try {
-                const parsedSchedules = JSON.parse(rawSchedules);
-                FS.writeFileSync('./resources/.cache/spl2-salmonrun.json', JSON.stringify(parsedSchedules, null, 2));
-                var timeEndSR = new Date(parsedSchedules.details[0].end_time);
-                let SRstage = parsedSchedules.details[0].stage;
-                let SRw1 = parsedSchedules.details[0].weapons[0].id > -1 ? parsedSchedules.details[0].weapons[0].weapon.name : `*${parsedSchedules.details[0].weapons[0].coop_special_weapon.name}*`;
-                let SRw2 = parsedSchedules.details[0].weapons[1].id > -1 ? parsedSchedules.details[0].weapons[1].weapon.name : `*${parsedSchedules.details[0].weapons[1].coop_special_weapon.name}*`;
-                let SRw3 = parsedSchedules.details[0].weapons[2].id > -1 ? parsedSchedules.details[0].weapons[2].weapon.name : `*${parsedSchedules.details[0].weapons[2].coop_special_weapon.name}*`;
-                let SRw4 = parsedSchedules.details[0].weapons[3].id > -1 ? parsedSchedules.details[0].weapons[3].weapon.name : `*${parsedSchedules.details[0].weapons[3].coop_special_weapon.name}*`;
 
-                var timeRightNow = Math.floor(Date.now()/1000);
-                var timeLeftSR = Math.floor(Math.abs(timeEndSR - timeRightNow) /60/60);
-                let TSend = Date.now();
-                // Chariot.Logger.event("Current Turf War Stages: " + parsedSchedules.regular[0].stage_a.name + " & " + parsedSchedules.regular[0].stage_b.name);
-                // Chariot.Logger.event("Current Ranked " + parsedSchedules.gachi[0].rule.name + " Stages: " + parsedSchedules.gachi[0].stage_a.name + " & " + parsedSchedules.gachi[0].stage_b.name);
-                // Chariot.Logger.event("Current League " + parsedSchedules.league[0].rule.name + " Stages: " + parsedSchedules.league[0].stage_a.name + " & " + parsedSchedules.league[0].stage_b.name);
-                // Chariot.Logger.event("This rotation will change in: " + timeLeftBattle + " minutes");
-                if (timeRightNow < parsedSchedules.details[0].start_time) {
-                    message.channel.createEmbed(new Chariot.RichEmbed()
-                    .setColor('DARK_GREEN')
-                    .setTitle('Grizzco will open in **' + (Math.floor(Math.abs(parsedSchedules.details[0].start_time - timeRightNow) /60/60)) + '** hours.')
-                    .setFooter('Processed in ' + (TSend - TSbegin) + 'ms')
-                    );
-                    return; //Only continue further processing if there's a shift running
-                } else {
-                    message.channel.createEmbed(new Chariot.RichEmbed()
-                    .setColor('DARK_GREEN')
-                    .setTitle('Splatoon 2: Salmon Run Shift')
-                    .setDescription('This schedule is valid for the next **' + Math.floor(timeLeftSR) + '** hours')
-                    .setImage('https://splatoon2.ink/assets/splatnet' + SRstage.image)
-                    .addField('Stage', SRstage.name, true)
-                    .addField('Weapons', `${SRw1}\n${SRw2}\n${SRw3}\n${SRw4}`, true)
-                    .addField('Reward', '*soooon*', true)
-                    .setUrl('https://splatoon2.ink/')
-                    .setTimestamp()
-                    .setFooter('Data provided by Splatoon2.ink, processed in ' + (TSend - TSbegin) + 'ms','https://splatoon2.ink/favicon-32x32.png')
-                    );}
+        const reqSR = await axios.get('/data/coop-schedules.json', options);
+        const dataSR = reqSR.data;
+        if (cacheSRG == null || cacheSR.details[0].end_time < Date.now()/1000) {
+        const reqSRG = await axios.get('/data/timeline.json', options);
+        dataSRG = reqSRG.data;
+        FS.writeFileSync('./resources/.cache/spl2-srgear.json', JSON.stringify(dataSRG, null, 2));
+        } else {dataSRG = cacheSRG;};
+       
+        FS.writeFileSync('./resources/.cache/spl2-salmonrun.json', JSON.stringify(dataSR, null, 2));
+        var timeEndSR = new Date(dataSR.details[0].end_time);
+        let SRstage = dataSR.details[0].stage;
+        let SRw1 = dataSR.details[0].weapons[0].id > -1 ? dataSR.details[0].weapons[0].weapon.name : `*${dataSR.details[0].weapons[0].coop_special_weapon.name}*`;
+        let SRw2 = dataSR.details[0].weapons[1].id > -1 ? dataSR.details[0].weapons[1].weapon.name : `*${dataSR.details[0].weapons[1].coop_special_weapon.name}*`;
+        let SRw3 = dataSR.details[0].weapons[2].id > -1 ? dataSR.details[0].weapons[2].weapon.name : `*${dataSR.details[0].weapons[2].coop_special_weapon.name}*`;
+        let SRw4 = dataSR.details[0].weapons[3].id > -1 ? dataSR.details[0].weapons[3].weapon.name : `*${dataSR.details[0].weapons[3].coop_special_weapon.name}*`;
+        let SRreward = `${dataSRG.coop.reward_gear.gear.brand.name} ${dataSRG.coop.reward_gear.gear.name}`
 
-            } catch (e) { message.channel.createMessage("❌ Problem with SR request: `" + e.message + "`"); }
-            });
-
-        });
-        
-        req2.on('error', (e) => {
-            message.channel.createMessage("❌ Problem with SR request: `" + e.message + "`");
-            });
-
-        req2.end();
+        var timeRightNow = Math.floor(Date.now()/1000);
+        var timeLeftSR = Math.floor(Math.abs(timeEndSR - timeRightNow) /60/60);
+        let TSend = Date.now();
+        // Chariot.Logger.event("Current Turf War Stages: " + dataSR.regular[0].stage_a.name + " & " + dataSR.regular[0].stage_b.name);
+        // Chariot.Logger.event("Current Ranked " + dataSR.gachi[0].rule.name + " Stages: " + dataSR.gachi[0].stage_a.name + " & " + dataSR.gachi[0].stage_b.name);
+        // Chariot.Logger.event("Current League " + dataSR.league[0].rule.name + " Stages: " + dataSR.league[0].stage_a.name + " & " + dataSR.league[0].stage_b.name);
+        // Chariot.Logger.event("This rotation will change in: " + timeLeftBattle + " minutes");
+        if (timeRightNow < dataSR.details[0].start_time) {
+            message.channel.createEmbed(new Chariot.RichEmbed()
+            .setColor('DARK_GREEN')
+            .setTitle('Grizzco will open in **' + (Math.floor(Math.abs(dataSR.details[0].start_time - timeRightNow) /60/60)) + '** hours.')
+            .setFooter('Processed in ' + (TSend - TSbegin) + 'ms')
+            );
+            return; //Only continue further processing if there's a shift running
+        } else {
+            message.channel.createEmbed(new Chariot.RichEmbed()
+            .setColor('DARK_GREEN')
+            .setTitle('Splatoon 2: Salmon Run Shift')
+            .setDescription('This schedule is valid for the next **' + Math.floor(timeLeftSR) + '** hours')
+            .setImage('https://splatoon2.ink/assets/splatnet' + SRstage.image)
+            .addField('Stage', SRstage.name, true)
+            .addField('Weapons', `${SRw1}\n${SRw2}\n${SRw3}\n${SRw4}`, true)
+            .addField('Reward', SRreward, true)
+            .setUrl('https://splatoon2.ink/')
+            .setTimestamp()
+            .setFooter('Data provided by Splatoon2.ink, processed in ' + (TSend - TSbegin) + 'ms','https://splatoon2.ink/favicon-32x32.png')
+            );}
 
         } else { // Use the cached data
+        if (cacheSRG == null || cacheSR.details[0].end_time < Date.now()/1000) {
+            const reqSRG = await axios.get('/data/timeline.json', options);
+            dataSRG = reqSRG.data;
+            FS.writeFileSync('./resources/.cache/spl2-srgear.json', JSON.stringify(dataSRG, null, 2));
+            } else {dataSRG = cacheSRG;};
         let SRstage = cacheSR.details[0].stage;
         let SRw1 = cacheSR.details[0].weapons[0].id > -1 ? cacheSR.details[0].weapons[0].weapon.name : `*${cacheSR.details[0].weapons[0].coop_special_weapon.name}*`;
         let SRw2 = cacheSR.details[0].weapons[1].id > -1 ? cacheSR.details[0].weapons[1].weapon.name : `*${cacheSR.details[0].weapons[1].coop_special_weapon.name}*`;
         let SRw3 = cacheSR.details[0].weapons[2].id > -1 ? cacheSR.details[0].weapons[2].weapon.name : `*${cacheSR.details[0].weapons[2].coop_special_weapon.name}*`;
         let SRw4 = cacheSR.details[0].weapons[3].id > -1 ? cacheSR.details[0].weapons[3].weapon.name : `*${cacheSR.details[0].weapons[3].coop_special_weapon.name}*`;
+        let SRreward = `${dataSRG.coop.reward_gear.gear.brand.name} ${dataSRG.coop.reward_gear.gear.name}`
         var timeEndSR = new Date(cacheSR.details[0].end_time);
         var timeRightNow = Math.floor(Date.now()/1000);
         var timeLeftSR = Math.floor(Math.abs(timeEndSR - timeRightNow) /60/60);
@@ -192,7 +165,7 @@ class Splatoon extends Chariot.Command {
             .setImage('https://splatoon2.ink/assets/splatnet' + SRstage.image)
             .addField('Stage', SRstage.name, true)
             .addField('Weapons', `${SRw1}\n${SRw2}\n${SRw3}\n${SRw4}`, true)
-            .addField('Reward', '*soooon*', true)
+            .addField('Reward', SRreward, true)
             .setUrl('https://splatoon2.ink/')
             .setTimestamp()
             .setFooter('Cached data provided by Splatoon2.ink, processed in ' + (TSend - TSbegin) + 'ms','https://splatoon2.ink/favicon-32x32.png')
