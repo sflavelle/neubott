544a6c5236ccc14c9ecd822e098abc5527311c76
Simon Flavelle <me@neurario.com>
2020-02-15 00:40:21 +0000

    This is ugly but, this will have to do for now

--- events/commandaliases.js
+++ events/commandaliases.js
@@ -9,6 +9,26 @@ class CommandAliases extends Chariot.Event {
     }
 
     async execute(message, args, chariot) {
+        if (message.author.id === '349097407998459916') {// Telegram Bot
+            if (message.content.search(/\ngimme squids$/i) != -1 || message.content.search(/\n🦑 ?📷$/) != -1) {
+                const AliasedCommand = this.client.commands.get('gimmesquids');
+                AliasedCommand.execute(message, args, chariot);
+            } 
+    
+            if (message.content.search(/\nwhat (else )?did I miss\??$/i) != -1 || message.content.includes('what I missed')) {
+                const AliasedCommand = this.client.commands.get('whatdidimiss');
+                AliasedCommand.execute(message, args, chariot);
+            } 
+    
+            if (message.content.search(/\n(bott? ?|bucket |neubott )?facts$/i) != -1) {
+                const AliasedCommand = this.client.commands.get('facts');
+                AliasedCommand.execute(message, args, chariot);
+            } 
+            return;
+        }
+
+        // For everyone else
+
         if (message.content.search(/^gimme squids$/i) != -1 || message.content.search(/^🦑 ?📷$/) != -1) {
             const AliasedCommand = this.client.commands.get('gimmesquids');
             AliasedCommand.execute(message, args, chariot);
