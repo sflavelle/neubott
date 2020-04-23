f4a9159f405d307c2bf720e30c12d4c6118780fa
Simon Flavelle <me@neurario.com>
2020-04-23 12:02:55 +0000

    I've got somewhere!

--- events/idlesystem.js
+++ events/idlesystem.js
@@ -11,23 +11,41 @@ class IdleSystem extends Chariot.Event {
     
     async execute(message) {
 
+        if (!this.client.idle) { this.client.idle = []; }
+
         function IdleContent(message) {
-            message.channel.createMessage("Test");
+            let deaths = ( FS.existsSync('./resources/idle/deaths.json') ) ? JSON.parse(FS.readFileSync('./resources/idle/deaths.json', 'utf8')) : null;
+            let user = (message.member.nick) ? message.member.nick : message.author.username;
+            let server = message.channel.guild.name;
+
+
+            
+            let deathMessage = deaths[Math.floor(Math.random()*deaths.length)];
+            // Substitute variable names if necessary
+            deathMessage = deathMessage.replace("`user`", user);
+            deathMessage = deathMessage.replace("`server`", server);
+            deathMessage = deathMessage.replace("`USER`", user.toUpperCase());
+            deathMessage = deathMessage.replace("`SERVER`", server.toUpperCase());
+
+            message.channel.createMessage("💀 " + deathMessage);
             Chariot.Logger.event(`[Idle] executed idle routine in ${message.channel.name}`);
         }
 
-        switch (message.channel.id) {
-            case "206734382990360576": // #meep
-            case "383151258065698816": // #bott-playground
-                Chariot.Logger.event(`[Idle] new message in ${message.channel.name}`);
-                let shortTimer = setTimeout(IdleContent, 1000*60, message);
-                break;
-            case "538503736423612426": // #splatoon
-            default:
-                break;
+        if (!message.author.bot) {
+            switch (message.channel.id) {
+                case "206734382990360576": // #meep
+                case "383151258065698816": // #bott-playground
+                    clearTimeout(this.client.idle[message.channel.id]); //I have to clear the timeout, before I set it...
+                    Chariot.Logger.event(`[Idle] new message in ${message.channel.name}`);
+                    this.client.idle[message.channel.id] = setTimeout(IdleContent, 45*1000, message); //45 minutes
+                    break;
+                case "538503736423612426": // #splatoon
+                default:
+                    break;
+            }
         }
         
     }
 }
 
-//module.exports = new IdleSystem(); // Don't let this run for now
+module.exports = new IdleSystem(); // Don't let this run for now
