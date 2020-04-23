1bb400f37f84367a713b1c67a62b179b618f5bb1
Simon Flavelle <me@neurario.com>
2020-04-23 11:08:51 +0000

    Still attempting work on this

--- events/idlesystem.js
+++ events/idlesystem.js
@@ -1,4 +1,5 @@
 const Chariot = require('chariot.js');
+const FS = require("fs");
 
 class IdleSystem extends Chariot.Event {
     /**
@@ -8,29 +9,25 @@ class IdleSystem extends Chariot.Event {
         super('messageCreate');
     }
     
+    async execute(message) {
 
-    async execute(message, chariot) {
-        // List of array files
-        let choices = [];
-        choices[0] = 'facts.json';
-        choices[1] = 'splat.json';
-
-        // Special conditions for certain channels
-        if (message.channel.id === 538503736423612426) // #splatoon
-        { let TheChoice = JSON.parse(FS.readFileSync('./resources/' + choices[1], 'utf8')); } // Always use the squids file
-        else 
-        { let TheChoice = JSON.parse(FS.readFileSync('./resources/' + choices[Math.floor(Math.random()*choices.length)], 'utf8')); }
-
-        idlemsg = TheChoice[Math.floor(Math.random()*TheChoice.length)];
-        switch (message.channel.id) {
-            case 206734382990360576: // #meep
-            case 383151258065698816: // #bott-playground
-            case 538503736423612426: // #splatoon
-            default:
+        function IdleContent(message) {
+            message.channel.createMessage("Test");
+            Chariot.Logger.event(`[Idle] executed idle routine in ${message.channel.name}`);
         }
-        message.channel.createMessage()
+
+        switch (message.channel.id) {
+            case "206734382990360576": // #meep
+            case "383151258065698816": // #bott-playground
+                Chariot.Logger.event(`[Idle] new message in ${message.channel.name}`);
+                let shortTimer = setTimeout(IdleContent, 1000*60, message);
+                break;
+            case "538503736423612426": // #splatoon
+            default:
+                break;
+        }
         
     }
 }
 
-// module.exports = new IdleSystem(); // Don't let this run for now
+//module.exports = new IdleSystem(); // Don't let this run for now
