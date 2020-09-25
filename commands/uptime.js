4f99b409bc30309f8adc86b80d7f51dee9f147f3
Simon Flavelle <me@neurario.com>
2020-09-25 07:24:30 +0000

    uptime text formatting

--- commands/uptime.js
+++ commands/uptime.js
@@ -9,7 +9,7 @@ module.exports = {
       usage: [ 'uptime' ],
     },
     execute(message, args) {
-      message.channel.send(`${this.icon} This process has been running for ${humanizeDuration(process.uptime()*1000, { round: true, maxDecimalPoints:1 })}`);
+      message.channel.send(`${this.icon} This process has been running for **${humanizeDuration(process.uptime()*1000, { round: true, maxDecimalPoints:1 })}**!`);
     },
     idle(message, args) {
       message.channel.send(`${this.icon} I've been running for **${humanizeDuration(process.uptime()*1000, { round: true, maxDecimalPoints:1, largest: 2 })}** so far!`);
